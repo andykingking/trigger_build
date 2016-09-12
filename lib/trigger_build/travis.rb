20033a7ba6fd06d2cd12b0a48dd41af27c6299d8
Ryan Davis <void@rdavis.xyz>
2016-09-12 14:11:08 +1000

    Add http request method to trigger travis build

---
+++ lib/trigger_build/travis.rb
@@ -0,0 +1,23 @@
+require 'httparty'
+
+module TriggerBuild
+
+  class Travis
+    include HTTParty
+    headers 'Accept' => 'application/json',
+      'Content-Type' => 'application/json',
+      'Travis-API-Version' => '3'
+
+    def initialize(opts)
+      self.class.base_uri "api.#{opts[:url]}/repo/#{opts[:owner]}\%2F#{opts[:repo]}"
+      self.class.headers 'Authorization' => "token #{opts[:token]}"
+    end
+
+    def trigger(message, branch: 'master')
+      self.class.post("/requests", query: {
+        request: { message: message, branch: branch }
+      })
+    end
+  end
+
+end
