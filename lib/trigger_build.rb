352fa1d3ffeb490cd44afb6947873b119e75dfed
Ryan Davis <void@rdavis.xyz>
2016-09-12 14:13:59 +1000

    Trigger travis build using given command line args

--- lib/trigger_build.rb
+++ lib/trigger_build.rb
@@ -1,9 +1,12 @@
 require 'trigger_build/options'
+require 'trigger_build/travis'
 
 module TriggerBuild
 
   def self.travis(args: ARGV)
     opts = Options.parse(args)
+    travis = Travis.new(opts)
+    travis.trigger('Testing dependency update triggers build', branch: 'master')
   end
 
 end
